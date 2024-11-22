2eb0456cb142fef2146a332766c446f7c6b62542
Gurpreet Gumber <gumber.gurpreet@rwit.io>
2024-11-22 17:37:41 +0530

    Integrated login form

---
+++ components/auth/login.php
@@ -0,0 +1,23 @@
+<form class="form-login" action="" method="post">
+    <div class="card-body">
+        <div class="form-group row mb-2">
+            <label for="inputEmail3" class="col-12 col-form-label">Email</label>
+            <div class="col-12">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+            </div>
+        </div>
+        <div class="form-group row mb-4">
+            <label for="inputPassword3" class="col-12 col-form-label">Password</label>
+            <div class="col-12">
+                <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+            </div>
+        </div>
+    </div>
+    <!-- /.card-body -->
+    <div class="card-footer">
+        <button type="submit" class="btn btn-primary me-2">Sign in</button>
+        <a href="" for="exampleCheck2">Forget Password?</a>
+        
+    </div>
+    <!-- /.card-footer -->
+</form>
--- index.php
+++ index.php
@@ -3,16 +3,34 @@ require './config.php';
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./assets/css/main.min.css" rel="stylesheet">
-</head>
-<body>
-    hello world
-    <script src="./assets/js/popper.min.js"></script>
-    <script src="./assets/js/main.min.js"></script>
-</body>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" id="bootstrap-css" href="./assets/css/main.min.css">
+        <link rel="stylesheet" id="custom-css" href="./assets/css/custom.min.css">
+        <link rel="shortcut icon" href="./assets/images/heart.svg" type="image/x-icon">
+    </head>
+
+    <body id="main" class="bg-light">
+        <main class="container content d-flex flex-column justify-content-center align-items-center">
+            <div class="logo d-flex flex-row align-items-center">
+                <img class="me-2" src="./assets/images/heart.svg" alt="hm" width="56" height="56">
+                <span class="fs-2 fw-bold">HM</span>
+            </div>
+            <div class="form">
+                <div class="form-container">
+                    <?php require './components/auth/login.php' ?>
+                </div>
+            </div>
+        </main>
+        <footer>
+
+        </footer>
+        <script id="popper-js" src="./assets/js/popper.min.js"></script>
+        <script id="main-js" src="./assets/js/main.min.js"></script>
+    </body>
+
 </html>
