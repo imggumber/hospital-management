23e6973365c670d7c8012699a83c31d2bd002f95
Gurpreet Gumber <gumber.gurpreet@rwit.io>
2024-11-22 15:35:30 +0530

    Prepare core structure

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
+    die;
+}
+
+// core files
+require './core/config.php';
--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+require './config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/main.min.css" rel="stylesheet">
+</head>
+<body>
+    hello world
+    <script src="./assets/js/popper.min.js"></script>
+    <script src="./assets/js/main.min.js"></script>
+</body>
+</html>
