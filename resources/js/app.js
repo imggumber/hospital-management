93dcd81c829b1ba4d5d6cc337540ed641ce8016d
Gurpreet Gumber <gumber.gurpreet@rwit.io>
2024-11-28 15:23:29 +0530

    Integrated vuetify css framework

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,8 @@
 import "./bootstrap";
 import router from "./router";
 import { createApp } from "vue";
+import vuetify from "./vuetify";
 
 import App from "./App.vue";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(vuetify).mount("#app");
