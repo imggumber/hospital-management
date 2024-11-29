9b7ec4c127e353004c7d7af818530137e52639a5
Gurpreet Gumber <gumber.gurpreet@rwit.io>
2024-11-29 13:24:33 +0530

    Added bootstrap

--- resources/js/Pages/HomeRoute.vue
+++ resources/js/Pages/HomeRoute.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
+  <div class="container">
+    <div class="container-content">
+      
+    </div>
+    <LoginForm/>
+  </div>
+
+  <!-- <div>
     <h1>HOME</h1>
     <router-link to="/test">Take me to Test page</router-link>
     <button @click.prevent="triggerEndpoint">Trigger Endpoint</button>
     <p v-if="response">{{ response }}</p>
-  </div>
+  </div> -->
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
+import LoginForm from '../Components/LoginForm.vue';
 
 // Define a ref to store the response data
 const response = ref(null);
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,7 @@
 import "./bootstrap";
 import router from "./router";
 import { createApp } from "vue";
-import vuetify from "./vuetify";
 
 import App from "./App.vue";
 
-createApp(App).use(router).use(vuetify).mount("#app");
+createApp(App).use(router).mount("#app");
