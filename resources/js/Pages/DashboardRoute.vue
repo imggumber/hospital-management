ef3289401e5c8b764fa6f581471d87ce1f67f3d1
Gurpreet Gumber <gumber.gurpreet@rwit.io>
2024-12-04 13:49:25 +0530

    Added 4 card components

--- resources/js/Pages/DashboardRoute.vue
+++ resources/js/Pages/DashboardRoute.vue
@@ -1,6 +1,17 @@
 <template>
     <DashboardLayout>
-        <Card/>
+        <div class="contentarea">
+            <section class="cards">
+                <div class="cards__cont">
+                    <div class="cards__cont__content">
+                        <Card title="OPD" count="54" backgroundColor="bg-warning" />
+                        <Card title="Current Patient" count="3" backgroundColor="bg-primary" />
+                        <Card title="Admission" count="74" backgroundColor="bg-success-subtle" />
+                        <Card title="On Leaves" count="6" backgroundColor="bg-success" />
+                    </div>
+                </div>
+            </section>
+        </div>
     </DashboardLayout>
 </template>
 
